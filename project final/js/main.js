09f0ebaf037c9ed1d4485a2e0d1bd3068e834dc1
ahmedSadoun <ahmedsaadoun866@gmail.com>
2024-10-02 21:18:29 +0200

    added guides section to front-end

--- project final/js/main.js
+++ project final/js/main.js
@@ -3,15 +3,16 @@ $(document).ready(async function () {
   let params = extractParams();
   let category = params.get("category");
   let categoryObject = await getItemsDescriptionByCategory(category);
-  let categoryItems = await getDataByWikisCategory(category);
-  console.log("categoryObject ", categoryObject);
-
-  console.log("result ", categoryItems);
+  let categoryItems = [];
+  if (categoryObject.children && categoryObject.children.length > 0) {
+    categoryItems = await getDataByWikisCategory(category);
+  }
   renderPageSections(categoryItems, categoryObject);
 });
 function renderPageSections(categoryItems, categoryObject) {
   renderPageTitleSection(categoryObject);
   renderItems(categoryItems);
+  renderGuides(categoryObject.guides);
 }
 // Page Title Begin
 function renderPageTitleSection(categoryObject) {
@@ -25,12 +26,26 @@ function renderPageTitleSection(categoryObject) {
 
 function renderItems(list) {
   let result = buildDevicesItemsGrid(list);
-
   document.getElementById("itemsContainer").innerHTML = "";
   document.getElementById("itemsContainer").innerHTML = result;
 }
 // Page Items ends
+// Page Guides Begin
 
+function renderGuides(list) {
+  let result = buildDevicesGuidesGrid(list);
+  document.getElementById("guidesContainer").innerHTML = "";
+  document.getElementById("guidesContainer").innerHTML = result;
+}
+// Page Guides ends
+
+function buildDevicesGuidesGrid(list) {
+  let itemsGridDesign = `<br/><br/><br/><h2 id="Section_Guides">Guides</h2><hr/><br/>`;
+  list.forEach((element) => {
+    itemsGridDesign += guidItemBuilder(element);
+  });
+  return itemsGridDesign;
+}
 function buildDevicesItemsGrid(list) {
   let itemsGridDesign = "";
   list.forEach((element) => {
@@ -51,6 +66,25 @@ function deviceItemBuilder(item) {
              alt="${item.display_title}">
         <div class="card-body">
           <h5 class="card-title">${item.display_title}</h5>
+        </div>
+      </a>
+    </div>
+  </div>`;
+  return itemDesign;
+}
+function guidItemBuilder(item) {
+  console.log("items sss ", item);
+  let itemDesign = ` <div class="col-md-4 mb-4">
+    <div class="card border-0 shadow-sm">
+      <a href="main.html?category=${item.title.replace(
+        " ",
+        "_"
+      )}" class="text-decoration-none">
+        <img src=${item.image?.standard}
+             class="card-img-top img-fluid rounded-2" 
+             alt="${item.title}">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
         </div>
       </a>
     </div>
